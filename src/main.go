d427d0c0fb15aa23353b30f19e7cb41285789d24
dojin.git@gmail.com <ec2-user@ip-172-31-6-40.us-east-2.compute.internal>
2021-06-16 11:57:16 +0000

    add github.com/dgrijalva/jwt-go

--- src/main.go
+++ src/main.go
@@ -6,5 +6,10 @@ import (
 )
 
 func main() {
-
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{"title": "index page"})
+	})
+	r.Run()
 }
